cd71e88e337b4dc4176a27dd3231a1ceadc69a73
jayOnTheStreet <taychakhun.r@gmail.com>
2025-07-03 15:09:01 +0700

    feat: add header component with user authentication and navigation links

---
+++ src/components/header.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { LucideKanban, LucideLogOut } from 'lucide-react'
+import Link from 'next/link'
+import { signOut } from '@/features/auth/actions/sign-out'
+import { useAuth } from '@/features/auth/hooks/use-auth'
+import { homePath, signInPath, signUpPath } from '@/paths'
+import { SubmitButton } from './form/submit-button'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { buttonVariants } from './ui/button'
+
+export const Header = () => {
+  const { user, isFetched } = useAuth()
+
+  if (!isFetched) {
+    return null
+  }
+
+  const navItems = user ? (
+    <form action={signOut}>
+      <SubmitButton label="Sign Out" icon={<LucideLogOut />} />
+    </form>
+  ) : (
+    <>
+      <Link
+        href={signUpPath()}
+        className={buttonVariants({ variant: 'outline' })}
+      >
+        Sign Up
+      </Link>
+      <Link
+        href={signInPath()}
+        className={buttonVariants({ variant: 'default' })}
+      >
+        Sign In
+      </Link>
+    </>
+  )
+
+  return (
+    <nav className="animate-header-from-top supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
+      <div className="flex align-items gap-x-2">
+        <Link
+          href={homePath()}
+          className={buttonVariants({ variant: 'ghost' })}
+        >
+          <LucideKanban />
+          <h1 className="text-lg font-semibold">TicketBounty</h1>
+        </Link>
+      </div>
+      <div className="flex align-items gap-x-2">
+        <ThemeSwitcher />
+        {navItems}
+      </div>
+    </nav>
+  )
+}
